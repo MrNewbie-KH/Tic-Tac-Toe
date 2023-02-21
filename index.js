9c934f7ea154ef8deacf427695070c556661713a
mohamed khattab <mkhattab369@gmail.com>
2023-02-21 03:37:47 +0200

    Clearing Now i can reset the board by clicking clear button after the game is done

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 "use strict";
 const cells = document.querySelectorAll(".cell");
+const container = document.querySelector(".container");
+const showUp = document.createElement("div");
+const showUpText = document.createElement("p");
+const clearBtn = document.createElement("button");
+clearBtn.textContent = "CLEAR";
+clearBtn.classList.add("clear-btn");
+showUp.appendChild(showUpText);
+showUp.classList.add("appear-up");
+const winner = document.createElement("span");
+winner.classList.add("winner");
 
 // ===================================================================
 // iife to build the game board once and only once bro
@@ -7,7 +17,10 @@ const cells = document.querySelectorAll(".cell");
 const gameBoard = (function () {
   const arrayBoard = [];
   for (let i = 0; i < 9; i++) arrayBoard[i] = i;
-  return { arrayBoard };
+  const clearCell = function () {
+    for (let i = 0; i < 9; i++) cells[i].textContent = "";
+  };
+  return { arrayBoard, clearCell };
 })();
 // ===================================================================
 // factory function to build players
@@ -26,12 +39,12 @@ const Player = function (name) {
 const checkWinner = function (arr) {
   for (let i = 0; i < 3; i++) {
     if (
-      (arr[i] === arr[i + 1] && arr[i] === arr[i + 2]) ||
+      (arr[i * 3] === arr[i * 3 + 1] && arr[i * 3] === arr[i * 3 + 2]) ||
       (arr[i] === arr[i + 3] && arr[i] === arr[i + 6]) ||
       (arr[0] === arr[4] && arr[0] === arr[8]) ||
       (arr[2] === arr[4] && arr[2] === arr[6])
     ) {
-      console.log("Win");
+      return true;
     }
   }
 };
@@ -47,11 +60,18 @@ const gameController = (function () {
   const increaseO = () => {
     ++o;
   };
+  const reset = () => {
+    x = 0;
+    o = 0;
+  };
+  const sum = () => {
+    return x + o;
+  };
   const increaseWho = () => {
     if (x > o) return "O";
     else return "X";
   };
-  return { increaseO, increaseX, increaseWho };
+  return { increaseO, increaseX, increaseWho, sum, reset };
 })();
 // ======================================================================
 // mainEvent
@@ -68,8 +88,38 @@ cells.forEach((cell) => {
         e.target.textContent = "O";
         gameBoard.arrayBoard[e.target.id.slice(-1) - 1] = "O";
       }
-      checkWinner(gameBoard.arrayBoard);
+      if (checkWinner(gameBoard.arrayBoard) === true) {
+        winner.textContent = element;
+        showUp.textContent = `<< The Winner is ... >> `;
+        showUp.appendChild(winner);
+        container.appendChild(showUp);
+        showUp.appendChild(clearBtn);
+      } else {
+        if (gameController.sum() === 9) {
+          winner.textContent = "Tie";
+          showUp.textContent = `<< It's a ... >>`;
+          showUp.appendChild(winner);
+          container.appendChild(showUp);
+          showUp.appendChild(clearBtn);
+        }
+      }
       e.target.classList.add("no-hover");
     }
   });
+});
+// ===================================================================
+// function to clear or reset the game
+// 1-reset array
+// 2-reset the appearing x and o
+// ===================================================================
+// const resetAll
+
+window.addEventListener("click", function (e) {
+  if (e.target.classList.contains("clear-btn")) {
+    gameBoard.arrayBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    gameBoard.clearCell();
+    gameController.reset();
+
+    container.removeChild(showUp);
+  }
 });
